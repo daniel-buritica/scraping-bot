1b054a3fe1388f4343ad9682102d2b75177197d5
daniel-buritica <buritica_1999@hotmail.com>
2023-10-21 10:54:27 -0500

    feat: proof

--- bots/farmatodo/FarmatodoImpl.py
+++ bots/farmatodo/FarmatodoImpl.py
@@ -7,11 +7,16 @@ def scraper(url):
     driver = setting_options_argument()
     driver.get(url)
     products = driver.find_elements(By.CSS_SELECTOR, "div.card-unique")
+
     for producto in products:
-        title = producto.find_element(By.CSS_SELECTOR, "p.text-title").text
-        price = producto.find_element(By.CSS_SELECTOR, "span.text-price").text
-        print(title)
-        print(price)
+        #title = producto.find_element(By.CSS_SELECTOR, "p.text-title").text
+        #price = producto.find_element(By.CSS_SELECTOR, "span.text-price").text
+
+        img = producto.find_element(By.CSS_SELECTOR, "img.image-virtual")
+        print(img.get_attribute("class"))
+        #print(title)
+        #print(price)
+        #print(img)
 
     driver.quit()
 
