1c2799b926e4c062f230677d8d743b489cf4b7ea
daniel-buritica <buritica_1999@hotmail.com>
2023-10-28 20:47:49 -0500

    feat: se termina cruz verde se añade folder de colsubsidio

---
+++ bots/colsubsidio/ColsubsidioImpl.py
@@ -0,0 +1,30 @@
+import time
+
+from config.database.RepositoryDB import insert_db
+from config.scrapper.ConfigWebDriverScrapper import setting_options_argument
+from selenium.webdriver.common.by import By
+from bots.farmatodo.util.MapperFarmaTodo import to_model
+
+
+def scraper(url):
+    driver = setting_options_argument()
+    driver.get(url)
+    time.sleep(10)
+    products = driver.find_elements(By.CSS_SELECTOR, "div.ImpreseeItem")
+
+
+    for product in products:
+        title = product.find_element(By.CSS_SELECTOR, "span.ImpreseeTitle").text
+        price = product.find_element(By.CSS_SELECTOR, "span.ImpreseePrice").text
+        img = product.find_element(By.CSS_SELECTOR, "img.ImpreseeItemImage")
+        img_url = img.get_attribute("src")
+        data = to_model(title, price, img_url)
+        insert_db(data)
+
+
+    driver.quit()
+
+
+
+scraper("https://www.drogueriascolsubsidio.com/?IText=bloqueador")
+
